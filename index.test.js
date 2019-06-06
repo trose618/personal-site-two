25fcf32dfdfdd2acc45c0e5d9274c34a567319e9
trose618 <trose618@gmail.com>
2019-06-06 13:01:59 -0400

    fixed puppeteer test

--- index.test.js
+++ index.test.js
@@ -8,18 +8,12 @@ test('should click around', async () => {
     })
 
     const page = await browser.newPage();
-
     await page.setViewport({ width: 360, height: 640 })
-
     await page.goto(
-        'http://127.0.0.1:5500/'
+        'http://localhost:3001/'
     );
-
-
     await page.click('.burger');
-
     await page.waitFor(3000);
-
     await browser.close()
 
-})
+}, 10000);
